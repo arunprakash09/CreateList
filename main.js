30d97df06afba6a56d1f3be8bb4045fc626f22ac
Arun Prakash G <66074345+arunprakash09@users.noreply.github.com>
2021-01-20 14:57:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function addItem() {
+  let type = document.getElementById("type").value;
+  let value = document.getElementById("value").value;
+  type = document.createElement(type);
+  type.appendChild(document.createTextNode(value));
+  document.getElementById("parent").appendChild(type);
+}
